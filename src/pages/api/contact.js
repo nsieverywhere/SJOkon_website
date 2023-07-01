160a59079272e241b2c8d313552ff54d2a049c95
unknown <nsikansimon@gmail.com>
2023-07-01 20:43:06 +0100

    working on it

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,44 +1,44 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    // Extract the email from the request body
-    const { name, email, message } = req.body;
+// export default function handler(req, res) {
+//   if (req.method === 'POST') {
+//     // Extract the email from the request body
+//     const { name, email, message } = req.body;
     
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'nsikansimon@gmail.com', 
-        pass: process.env.EMAIL_PASSWORD,
-      },
-        // tls: {
-        //   rejectUnauthorized: false,
-        // },
+//     const transporter = nodemailer.createTransport({
+//       service: 'gmail',
+//       auth: {
+//         user: 'nsikansimon@gmail.com', 
+//         pass: process.env.EMAIL_PASSWORD,
+//       },
+//         tls: {
+//           rejectUnauthorized: false,
+//         },
       
-    });
+//     });
 
-    // Define the email options
-    const mailOptions = {
-      from: `${email}`, // Replace with your Gmail email address
-      to: 'nsikansimon@gmail.com', // Replace with the recipient's email address
-      subject: `${name} has a New Message from S.J Okon Website`,
-      text: ` ${message} \n \n From: ${name}`,
-    };
+//     // Define the email options
+//     const mailOptions = {
+//       from: `${email}`, // Replace with your Gmail email address
+//       to: 'nsikansimon@gmail.com', // Replace with the recipient's email address
+//       subject: `${name} has a New Message from S.J Okon Website`,
+//       text: ` ${message} \n \n From: ${name}`,
+//     };
 
-    // Send the email using Nodemailer
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending email:', error);
-        res.status(500).json({ message: 'Failed to send email' });
-      } else {
-        console.log('Email sent:', info.response);
-        res.status(200).json({ message: 'Email has been sent successfully.' });
-      }
-    });
-  } else {
-    // Return an error response for unsupported request methods
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
+//     // Send the email using Nodemailer
+//     transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) {
+//         console.error('Error sending email:', error);
+//         res.status(500).json({ message: 'Failed to send email' });
+//       } else {
+//         console.log('Email sent:', info.response);
+//         res.status(200).json({ message: 'Email has been sent successfully.' });
+//       }
+//     });
+//   } else {
+//     // Return an error response for unsupported request methods
+//     res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+// }
 
     
